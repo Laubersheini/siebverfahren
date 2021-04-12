2fd86f927e78956a92424b614651d5f8e37e8449
Laubersheini <d.laubersheimer@t-online.de>
2021-04-12 20:47:00 +0200

    colors now assinged randomly, added border around the prime numbers

--- script.js
+++ script.js
@@ -20,14 +20,23 @@ let squareroot = Math.sqrt(numberAmmount)
 for(let i=2;i<=squareroot;i++){
   if(!numbersCrossed[i]){
     for(let j= 2*i;j<=numberAmmount;j+=i){
+      let randomColor = "rgba("+Math.random()*255+","+Math.random()*255+","+Math.random()*255+",1)"
       console.log(i);
 
       if(!numbersCrossed[j]){
-      nodes[j-1].style.backgroundColor ="rgba("+colorAmmount*25+",0,100,1)"
+        nodes[j-1].style.backgroundColor = randomColor;
+
       }
       numbersCrossed[j] = true;
     }
     colorAmmount++
   }
 
+}
+
+
+for (var i = 1; i < numbersCrossed.length; i++) {
+  if(!numbersCrossed[i]){
+    nodes[i-1].style.border = "2px solid black"
+  }
 }
